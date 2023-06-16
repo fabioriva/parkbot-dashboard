d35324c7ac4d55361585928731c8c0119dcfe3c9
Fabio Riva <friva@LAPTOP-0T9EOJCD>
2023-06-16 15:07:41 +0200

    use AccordionList for Motor component

--- src/components/DeviceView.jsx
+++ src/components/DeviceView.jsx
@@ -1,26 +1,16 @@
-import { TabList, Tab, TabGroup, TabPanels, TabPanel } from "@tremor/react";
+import {
+  TabList,
+  Tab,
+  TabGroup,
+  TabPanels,
+  TabPanel,
+  AccordionList,
+} from "@tremor/react";
 import Motor from "@/components/Motor";
 // import Silomat from "@/components/Silomat";
 
 export default function View({ data }) {
   // console.log(data);
-  // const actuators = [
-  //   {
-  //     name: "Motor 3",
-  //     io: data.e.slice(6, 9),
-  //     status: "Locked",
-  //     encoders: [],
-  //   },
-  // ];
-  // const motors = [
-  //   {
-  //     name: "Motor 1",
-  //     io: data.e.slice(0, 2),
-  //     status: "Going up",
-  //     encoders: data.b.slice(2, 3),
-  //   },
-  // ];
-
   return (
     <TabGroup>
       <TabList className="mt-3">
@@ -31,31 +21,30 @@ export default function View({ data }) {
       <TabPanels>
         {data.main.length > 0 && (
           <TabPanel>
-            <div className="mt-3">
+            <AccordionList className="max-w-full mx-auto mt-3">
               {data.main.map((item, key) => (
                 <Motor item={item} key={key} />
               ))}
-            </div>
+            </AccordionList>
           </TabPanel>
         )}
 
         {data.silomat !== undefined && (
           <TabPanel>
-            <div className="mt-3">
-              {/* <Silomat data={data.silomat} /> */}
+            <AccordionList className="max-w-full mx-auto mt-3">
               {data.silomat.motors.map((item, key) => (
                 <Motor item={item} key={key} />
               ))}
-            </div>
+            </AccordionList>
           </TabPanel>
         )}
         {data.more.length > 0 && (
           <TabPanel>
-            <div className="mt-3">
+            <AccordionList className="max-w-full mx-auto mt-3">
               {data.more.map((item, key) => (
                 <Motor item={item} key={key} />
               ))}
-            </div>
+            </AccordionList>
           </TabPanel>
         )}
         {/* <TabPanel>
--- src/components/Motor.jsx
+++ src/components/Motor.jsx
@@ -1,23 +1,48 @@
-import { Card, Flex, Text } from "@tremor/react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Card,
+  Flex,
+  Text,
+} from "@tremor/react";
 import Io from "@/components/DeviceIo";
 import Position from "@/components/Position";
 
 export default function Motor({ item }) {
   // console.log(item);
   return (
-    <Card className="mt-3 p-3">
-      <Flex>
-        <div className="text-sm">{item.name}</div>
-        <Text>{item.status}</Text>
-      </Flex>
-      <div class="grid grid-cols-6 gap-3 mt-3">
-        {item.io.map((item, key) => (
-          <Io item={item} key={key} />
+    <Accordion>
+      <AccordionHeader>
+        <Flex>
+          <div className="text-sm">{item.name}</div>
+          <Text>{item.status}</Text>
+        </Flex>
+      </AccordionHeader>
+      <AccordionBody>
+        <div class="grid grid-cols-6 gap-3">
+          {item.io.map((item, key) => (
+            <Io item={item} key={key} />
+          ))}
+        </div>
+        {item.encoders.map((item, key) => (
+          <Position item={item} key={key} />
         ))}
-      </div>
-      {item.encoders.map((item, key) => (
-        <Position item={item} key={key} />
-      ))}
-    </Card>
+      </AccordionBody>
+    </Accordion>
+    // <Card className="mt-3 p-3">
+    //   <Flex>
+    //     <div className="text-sm">{item.name}</div>
+    //     <Text>{item.status}</Text>
+    //   </Flex>
+    //   <div class="grid grid-cols-6 gap-3 mt-3">
+    //     {item.io.map((item, key) => (
+    //       <Io item={item} key={key} />
+    //     ))}
+    //   </div>
+    //   {item.encoders.map((item, key) => (
+    //     <Position item={item} key={key} />
+    //   ))}
+    // </Card>
   );
 }
