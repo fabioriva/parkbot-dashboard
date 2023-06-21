fd8b183b657a7b55f913bd7b03df76d27aa21851
Fabio Riva <friva@LAPTOP-0T9EOJCD>
2023-06-21 14:36:43 +0200

    DeviceView component cleaning

--- src/components/DeviceView.jsx
+++ src/components/DeviceView.jsx
@@ -7,13 +7,11 @@ import {
   TabPanel,
   AccordionList,
 } from "@tremor/react";
-// import Drive from "@/components/Drive";
 import Motor from "@/components/Motor";
-// import Silomat from "@/components/Silomat";
 
 export default function View({ data }) {
-  // console.log(data);
   const [index, setIndex] = useState(0);
+
   useEffect(() => {
     console.log(data.a);
     setIndex(data.a.motor);
@@ -25,11 +23,7 @@ export default function View({ data }) {
         {data.motors !== undefined && data.motors.length > 0 && (
           <Tab>Motors</Tab>
         )}
-        {/* {data.more !== undefined && data.more.length > 0 && <Tab>More</Tab>} */}
         {data.silomat !== undefined && <Tab>Silomat</Tab>}
-        {/* {data.drives !== undefined && data.drives.length > 0 && (
-          <Tab>Drives</Tab>
-        )} */}
       </TabList>
       <TabPanels>
         {data.motors !== undefined && data.motors.length > 0 && (
@@ -41,15 +35,6 @@ export default function View({ data }) {
             </AccordionList>
           </TabPanel>
         )}
-        {/* {data.more !== undefined && data.more.length > 0 && (
-          <TabPanel>
-            <AccordionList className="max-w-full mx-auto mt-3">
-              {data.more.map((item, key) => (
-                <Motor item={item} key={key} />
-              ))}
-            </AccordionList>
-          </TabPanel>
-        )} */}
         {data.silomat !== undefined && (
           <TabPanel>
             <AccordionList className="max-w-full mx-auto mt-3">
@@ -59,37 +44,6 @@ export default function View({ data }) {
             </AccordionList>
           </TabPanel>
         )}
-        {/* {data.drives !== undefined && data.drives.length > 0 && (
-          <TabPanel>
-            <AccordionList className="max-w-full mx-auto mt-3">
-              {data.drives.map((item, key) => (
-                <Drive item={item} key={key} />
-              ))}
-            </AccordionList>
-          </TabPanel>
-        )} */}
-        {/* <TabPanel>
-          <div className="mt-3">
-            {data.b.map((item, key) => (
-              <Position item={item} key={key} />
-            ))}
-            {data.main.length > 0 &&
-              data.main.map((item, key) => <Motor item={item} key={key} />)}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="mt-3">
-            {data.more.map((item, key) => (
-              <Motor item={item} key={key} />
-            ))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="mt-3">
-            {data.e.length > 0 && <Silomat data={data.e} />}
-            {data.silomat !== undefined && <Silomat data={data.silomat} />}
-          </div>
-        </TabPanel> */}
       </TabPanels>
     </TabGroup>
   );
