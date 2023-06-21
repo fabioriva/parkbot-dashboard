6c10f532715b4ae4b516954a3b36f6a8b9d9d5be
Fabio Riva <friva@LAPTOP-0T9EOJCD>
2023-06-21 16:06:48 +0200

    added missing locales to map page

--- src/app/[locale]/[aps]/map/components/18017K.jsx
+++ src/app/[locale]/[aps]/map/components/18017K.jsx
@@ -1,22 +1,28 @@
 "use client";
 
+import { useTranslations } from "next-intl";
 import { Grid, Col, Flex, Card, Title } from "@tremor/react";
 import { useMap } from "@/hooks/useMap";
 
 export default function Map(props) {
   // console.log(props);
   const { Levels, Occupancy, View } = useMap(props);
+  const t = useTranslations("Map");
+
   return (
     <Grid numItemsLg={3} className="gap-6">
       <Col numColSpan={1} numColSpanLg={2}>
         <Card className="p-3 sm:p-6">
           <Flex>
-            <div>
-              <Title>System map</Title>
-            </div>
+            <Title>{t("title")}</Title>
             {View}
           </Flex>
-          <Grid numItems={1} numItemsMd={2} numItemsLg={3} className="gap-6 mt-3">
+          <Grid
+            numItems={1}
+            numItemsMd={2}
+            numItemsLg={3}
+            className="gap-6 mt-3"
+          >
             {Levels.map((level) => level)}
           </Grid>
           <style jsx global>
