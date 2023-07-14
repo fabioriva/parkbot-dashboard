36543d1452e69ef5427c3bfe9bc35d530e628859
Fabio Riva <friva@LAPTOP-0T9EOJCD>
2023-07-14 10:00:55 +0200

    new advanced view toggler

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { useTranslations } from "next-intl";
-import { WrenchIcon } from "@heroicons/react/24/solid";
-import { Badge, Card, Flex, Title } from "@tremor/react";
+import { EyeIcon, EyeSlashIcon, WrenchIcon } from "@heroicons/react/24/solid";
+import { Badge, Card, Flex, Title, Icon } from "@tremor/react";
 import Entry from "@/components/OperationDialog";
 import Info from "@/components/DeviceInfo";
 import Position from "@/components/Position";
@@ -19,12 +20,16 @@ const Led = ({ children, color, input }) => (
 
 export default function Device({ advanced, aps, data, token, user }) {
   // console.log(data);
+  const [more, setMore] = useState(true);
   const t = useTranslations("Log");
   const { card, mode, motor, name, operation, size, stall, step, steps } =
     data.a;
   // const { card, mode, motor, name, operation, size, stall, step, steps } = data;
   const [LS, LC, LA] = data.c;
-
+  const toggleAdvanced = (e) => {
+    e.preventDefault();
+    setMore(!more);
+  };
   return (
     <Card className="max-w-sm p-3 self-start">
       <Flex>
@@ -46,19 +51,33 @@ export default function Device({ advanced, aps, data, token, user }) {
           <Led color="green" input={LS}>
             LS
           </Led>
+          <button
+            type="button"
+            className="hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg"
+            onClick={toggleAdvanced}
+          >
+            <Icon icon={more ? EyeIcon : EyeSlashIcon} />
+          </button>
         </div>
       </Flex>
+
       <Info alarms={data.alarms} device={data.a} />
       {/* <Info alarms={data.alarms} device={data} /> */}
-      {advanced && data.b !== undefined && (
+
+      {advanced && more && data.b !== undefined && (
         <div className="mt-6">
           {data.b.map((item, key) => (
             <Position item={item} key={key} />
           ))}
         </div>
       )}
-      {advanced && <View data={data} />}
+      {advanced && more && (
+        // data.views !== undefined &&
+        // data.views.length > 0 &&
+        <View data={data} />
+      )}
       {advanced &&
+        more &&
         data.d.map((action, key) => {
           switch (action.key) {
             case "action-entry":
