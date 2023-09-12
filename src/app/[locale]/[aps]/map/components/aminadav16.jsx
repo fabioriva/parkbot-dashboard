07a37bd9308f2227e14ce7555bb280c36bef2ca5
Fabio Riva <f.riva@sotefin.ch>
2023-09-12 15:50:10 +0200

    map stylng

--- src/app/[locale]/[aps]/map/components/aminadav16.jsx
+++ src/app/[locale]/[aps]/map/components/aminadav16.jsx
@@ -21,269 +21,235 @@ export default function Map(props) {
           </Grid>
           <style jsx global>{`
             .l {
-              height: 160px;
-              width: 332px;
+              height: 98px;
+              width: 456px;
             }
-            #el-1 {
+            #eu-1 {
               top: 33px;
-              left: 2px;
+              left: 125px;
             }
-            #el-2 {
+            #eu-2 {
               top: 33px;
-              left: 43px;
+              left: 289px;
             }
             /* P1 */
             #s-1 {
-              top: 95px;
-              left: 289px;
+              top: 64px;
+              left: 2px;
             }
             #s-2 {
-              top: 33px;
-              left: 289px;
+              top: 2px;
+              left: 31px;
             }
             #s-3 {
-              top: 2px;
-              left: 289px;
+              top: 64px;
+              left: 43px;
             }
             #s-4 {
-              top: 33px;
-              left: 248px;
+              top: 2px;
+              left: 74px;
             }
             #s-5 {
-              top: 2px;
-              left: 248px;
+              top: 64px;
+              left: 84px;
             }
             #s-6 {
-              top: 95px;
-              left: 207px;
+              top: 2px;
+              left: 115px;
             }
             #s-7 {
-              top: 33px;
-              left: 207px;
+              top: 64px;
+              left: 125px;
             }
             #s-8 {
               top: 2px;
-              left: 207px;
+              left: 156px;
             }
             #s-9 {
-              top: 95px;
+              top: 64px;
               left: 166px;
             }
             #s-10 {
-              top: 33px;
-              left: 166px;
+              top: 64px;
+              left: 207px;
             }
             #s-11 {
-              top: 126px;
-              left: 125px;
+              top: 64px;
+              left: 248px;
             }
             #s-12 {
-              top: 95px;
-              left: 125px;
+              top: 2px;
+              left: 248px;
             }
             #s-13 {
-              top: 33px;
-              left: 125px;
+              top: 2px;
+              left: 289px;
             }
             #s-14 {
-              top: 2px;
-              left: 125px;
+              top: 64px;
+              left: 289px;
             }
             #s-15 {
-              top: 126px;
-              left: 84px;
+              top: 2px;
+              left: 330px;
             }
             #s-16 {
-              top: 95px;
-              left: 84px;
+              top: 64px;
+              left: 330px;
             }
             #s-17 {
-              top: 33px;
-              left: 84px;
+              top: 64px;
+              left: 371px;
             }
             #s-18 {
-              top: 2px;
-              left: 84px;
+              top: 64px;
+              left: 412px;
             }
+            /* P2 */
             #s-19 {
-              top: 126px;
-              left: 43px;
+              top: 64px;
+              left: 2px;
             }
             #s-20 {
-              top: 95px;
-              left: 43px;
+              top: 2px;
+              left: 31px;
             }
             #s-21 {
-              top: 33px;
+              top: 64px;
               left: 43px;
             }
             #s-22 {
               top: 2px;
-              left: 43px;
+              left: 74px;
             }
             #s-23 {
-              top: 33px;
-              left: 2px;
+              top: 64px;
+              left: 84px;
             }
             #s-24 {
               top: 2px;
-              left: 2px;
+              left: 115px;
             }
-
-            /* P2 */
             #s-25 {
-              top: 33px;
-              left: 289px;
+              top: 64px;
+              left: 125px;
             }
             #s-26 {
               top: 2px;
-              left: 289px;
+              left: 156px;
             }
             #s-27 {
-              top: 33px;
-              left: 248px;
+              top: 64px;
+              left: 166px;
             }
             #s-28 {
-              top: 2px;
-              left: 248px;
+              top: 64px;
+              left: 207px;
             }
             #s-29 {
-              top: 95px;
-              left: 207px;
+              top: 64px;
+              left: 248px;
             }
             #s-30 {
-              top: 33px;
-              left: 207px;
+              top: 2px;
+              left: 248px;
             }
             #s-31 {
               top: 2px;
-              left: 207px;
+              left: 289px;
             }
             #s-32 {
-              top: 95px;
-              left: 166px;
+              top: 64px;
+              left: 289px;
             }
             #s-33 {
-              top: 33px;
-              left: 166px;
+              top: 2px;
+              left: 330px;
             }
             #s-34 {
-              top: 126px;
-              left: 125px;
+              top: 64px;
+              left: 330px;
             }
             #s-35 {
-              top: 95px;
-              left: 125px;
+              top: 64px;
+              left: 371px;
             }
             #s-36 {
-              top: 33px;
-              left: 125px;
+              top: 64px;
+              left: 412px;
             }
+            /* P3 */
             #s-37 {
-              top: 2px;
-              left: 125px;
+              top: 64px;
+              left: 2px;
             }
             #s-38 {
-              top: 126px;
-              left: 84px;
+              top: 2px;
+              left: 31px;
             }
             #s-39 {
-              top: 95px;
-              left: 84px;
+              top: 64px;
+              left: 43px;
             }
             #s-40 {
-              top: 33px;
-              left: 84px;
+              top: 2px;
+              left: 74px;
             }
             #s-41 {
-              top: 2px;
+              top: 64px;
               left: 84px;
             }
             #s-42 {
-              top: 126px;
-              left: 43px;
+              top: 2px;
+              left: 115px;
             }
             #s-43 {
-              top: 95px;
-              left: 43px;
+              top: 64px;
+              left: 125px;
             }
-
-            /* P2 */
             #s-44 {
-              top: 33px;
-              left: 289px;
+              top: 2px;
+              left: 156px;
             }
             #s-45 {
-              top: 2px;
-              left: 289px;
+              top: 64px;
+              left: 166px;
             }
             #s-46 {
-              top: 33px;
-              left: 248px;
+              top: 64px;
+              left: 207px;
             }
             #s-47 {
-              top: 2px;
+              top: 64px;
               left: 248px;
             }
             #s-48 {
-              top: 95px;
-              left: 207px;
+              top: 2px;
+              left: 248px;
             }
             #s-49 {
-              top: 33px;
-              left: 207px;
+              top: 2px;
+              left: 289px;
             }
             #s-50 {
-              top: 2px;
-              left: 207px;
+              top: 64px;
+              left: 289px;
             }
             #s-51 {
-              top: 95px;
-              left: 166px;
+              top: 2px;
+              left: 330px;
             }
             #s-52 {
-              top: 33px;
-              left: 166px;
+              top: 64px;
+              left: 330px;
             }
             #s-53 {
-              top: 126px;
-              left: 125px;
+              top: 64px;
+              left: 371px;
             }
             #s-54 {
-              top: 95px;
-              left: 125px;
-            }
-            #s-55 {
-              top: 33px;
-              left: 125px;
-            }
-            #s-56 {
-              top: 2px;
-              left: 125px;
-            }
-            #s-57 {
-              top: 126px;
-              left: 84px;
-            }
-            #s-58 {
-              top: 95px;
-              left: 84px;
-            }
-            #s-59 {
-              top: 33px;
-              left: 84px;
-            }
-            #s-60 {
-              top: 2px;
-              left: 84px;
-            }
-            #s-61 {
-              top: 126px;
-              left: 43px;
-            }
-            #s-62 {
-              top: 95px;
-              left: 43px;
+              top: 64px;
+              left: 412px;
             }
           `}</style>
         </Card>
