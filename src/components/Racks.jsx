1ec132e4f3ae659f642571aee6ab4d96d35267f7
Fabio Riva <f.riva@sotefin.ch>
2023-10-06 09:43:34 +0200

    removed pn route

--- src/components/Racks.jsx
+++ src/components/Racks.jsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import Link from "next-intl/link";
 import { Fragment, useEffect, useState } from "react";
 import { Grid, Flex, Card, Text, Metric, Badge } from "@tremor/react";
-import Pn from "@/components/Profinet";
+import Pn from "@/components/Pn";
 import fetcher from "@/lib/fetch";
 import useSWR from "swr";
 
