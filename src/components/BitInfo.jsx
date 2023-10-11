5e38f63ad79d4a3c942ce9a09a84abc56c7546b4
Fabio Riva <f.riva@sotefin.ch>
2023-10-11 11:55:17 +0200

    refactor: build parameters for next-intl useTranslations hook

--- src/components/BitInfo.jsx
+++ src/components/BitInfo.jsx
@@ -1,17 +1,72 @@
 import { useTranslations } from "next-intl";
 
+function get(label) {
+  // console.log(label);
+  switch (label) {
+    case "AD1":
+    case "AD2":
+    case "AD3":
+    case "AF7":
+    case "AF8":
+    case "DB32":
+    case "FE12":
+    case "L1":
+    case "L2":
+    case "L3":
+    case "L4":
+    case "L5":
+    case "RS12":
+    case "RS3":
+    case "S1":
+    case "S2":
+    case "T10":
+    case "T2":
+      return { key: label, query: {} };
+
+    default:
+      // if (label) {
+      let array = label.split("");
+      const dash = array[array.length - 2];
+      if (dash === "-") {
+        const last = parseInt(array[array.length - 3]);
+        if (!isNaN(last)) {
+          return {
+            key: array.slice(0, -3).join("").concat("x"),
+            query: { nr: last },
+          };
+        } else {
+          return { key: label.slice(0, -2), query: {} };
+        }
+      }
+      const last = parseInt(array[array.length - 1]);
+      if (!isNaN(last)) {
+        return {
+          key: array.slice(0, -1).join("").concat("x"),
+          query: { nr: last },
+        };
+      } else {
+        return { key: label, query: {} };
+      }
+    // } else {
+    //   return { key: "label", query: {} };
+    // }
+  }
+}
+
 export default function BitInfo({ bit }) {
-  const t = useTranslations("Bit");
-  // const t = useTranslations("Rack");
+  // const t = useTranslations("Bit");
+  const t = useTranslations("Rack");
+  const { addr, label, status } = bit;
+  const { key, query } = get(label);
   const tooltip = (
     <ul>
-      <li>{bit.addr}</li>
-      <li>{bit.label}</li>
-      <li>{t(bit.label)}</li>
-      {/* <li>{bit.key ? t(bit.key, bit.query) : bit.label && t(bit.label)}</li> */}
-      <li className={bit.status ? "text-green-500" : "text-red-500"}>
-        {bit.status ? "ON" : "OFF"}
+      <li>{addr}</li>
+      <li>{label}</li>
+      <li className={status ? "text-green-500" : "text-red-500"}>
+        {status ? "ON" : "OFF"}
       </li>
+      {/* <li>{t(label)}</li> */}
+      <li>{key && t(key, query)}</li>
     </ul>
   );
   return tooltip;
