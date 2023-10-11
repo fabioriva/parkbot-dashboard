fec6e14e7abb04808f1b32d983342e0113b24eb0
Fabio Riva <f.riva@sotefin.ch>
2023-10-11 09:40:12 +0200

    refactor: new ICU mesage format

--- src/components/BitInfo.jsx
+++ src/components/BitInfo.jsx
@@ -2,11 +2,13 @@ import { useTranslations } from "next-intl";
 
 export default function BitInfo({ bit }) {
   const t = useTranslations("Bit");
+  // const t = useTranslations("Rack");
   const tooltip = (
     <ul>
       <li>{bit.addr}</li>
       <li>{bit.label}</li>
       <li>{t(bit.label)}</li>
+      {/* <li>{bit.key ? t(bit.key, bit.query) : bit.label && t(bit.label)}</li> */}
       <li className={bit.status ? "text-green-500" : "text-red-500"}>
         {bit.status ? "ON" : "OFF"}
       </li>
