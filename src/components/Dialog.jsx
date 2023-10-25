d08707d3adb9845ebbe7a870b9f6780424a43fff
Fabio Riva <f.riva@sotefin.ch>
2023-10-25 09:14:21 +0200

    Dialog component styling

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -36,7 +36,10 @@ export default function ModalDialog({ children, isOpen, title }) {
               leaveTo="opacity-0 scale-95"
             >
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-slate-50 dark:bg-slate-950 p-6 text-left align-middle shadow-xl transition-all">
-                <Dialog.Title as="h3" className="text-lg font-medium leading-6">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg dark:text-white font-medium leading-6"
+                >
                   {title}
                 </Dialog.Title>
                 {children}
