f17cccbbac405f22a4beaa3df915e9bb8a012cb8
Fabio Riva <f.riva@sotefin.ch>
2023-11-08 15:58:53 +0100

    move next-intl configuration file in /src

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const withNextIntl = require("next-intl/plugin")(
-  // This is the default (also the `src` folder is supported out of the box)
-  "./i18n.js"
-);
+const withNextIntl = require("next-intl/plugin")();
 
 const nextConfig = {};
 
