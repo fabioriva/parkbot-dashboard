4e1e2223173cf76d0cf02452a3be64384c6d9857
Fabio Riva <f.riva@sotefin.ch>
2024-11-15 16:14:47 +0100

    deleted unused comments

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,10 +1,8 @@
 import "../globals.css";
-
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
 import { notFound } from "next/navigation";
-// import { useLocale } from "next-intl";
 import { routing } from "@/i18n/routing";
 
 const inter = Inter({
@@ -19,17 +17,10 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children, params: { locale } }) {
-  // const locale = useLocale();
-  // Show a 404 error if the user requests an unknown locale
-  // if (params.locale !== locale) {
-  //   notFound();
-  // }
-
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale)) {
     notFound();
   }
-
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
@@ -44,32 +35,3 @@ export default async function RootLayout({ children, params: { locale } }) {
     </html>
   );
 }
-
-// import {notFound} from 'next/navigation';
-
-// export default async function LocaleLayout({
-//   children,
-//   params: {locale}
-// }: {
-//   children: React.ReactNode;
-//   params: {locale: string};
-// }) {
-//   // Ensure that the incoming `locale` is valid
-//   if (!routing.locales.includes(locale as any)) {
-//     notFound();
-//   }
-
-//   // Providing all messages to the client
-//   // side is the easiest way to get started
-//   const messages = await getMessages();
-
-//   return (
-//     <html lang={locale}>
-//       <body className={inter.className}>
-//         <NextIntlClientProvider messages={messages}>
-//           {children}
-//         </NextIntlClientProvider>
-//       </body>
-//     </html>
-//   );
-// }
