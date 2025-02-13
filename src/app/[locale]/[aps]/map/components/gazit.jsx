e01d2ac962b55addb4a06ce0b43c1d5994c0b18e
Fabio Riva <f.riva@sotefin.ch>
2025-02-13 10:46:58 +0100

    gazit map first commit

---
+++ src/app/[locale]/[aps]/map/components/gazit.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import { Grid, Col, Flex, Card, Title } from "@tremor/react";
+import { useMap } from "@/hooks/useMap";
+
+export default function Map(props) {
+  const { Levels, Occupancy, View } = useMap(props);
+  const t = useTranslations("Map");
+
+  return (
+    <Grid numItemsLg={3} className="gap-6">
+      <Col numColSpan={1} numColSpanLg={2}>
+        <Card className="p-3 sm:p-6">
+          <Flex>
+            <Title>{t("title")}</Title>
+            {View}
+          </Flex>
+          <Grid className="overflow-auto gap-6 mt-3">
+            {Levels.reverse().map((level) => level)}
+          </Grid>
+          <style jsx global>{`
+            .l {
+              height: 160px;
+              width: 333px;
+            }
+            #el-1 {
+              top: 95px;
+              left: 43px;
+            }
+          `}</style>
+        </Card>
+      </Col>
+      <Card className="max-w-sm self-start">{Occupancy}</Card>
+    </Grid>
+  );
+}
