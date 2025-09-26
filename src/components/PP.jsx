c1b30531d9b313f13b2c942b1146290b2087a579
Fabio Riva <f.riva@sotefin.ch>
2025-09-26 14:19:05 +0200

    PP component code refactoring

--- src/components/PP.jsx
+++ src/components/PP.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useState } from "react";
 import { useTranslations } from "next-intl";
-import { Button, Divider, Flex, Text, NumberInput } from "@tremor/react";
+import { Button, Flex, Text, NumberInput } from "@tremor/react";
 import Dialog from "@/components/Dialog";
 import Tooltip from "@/components/Tooltip";
 import fetch, { actionResponse } from "@/lib/fetch";
@@ -8,7 +8,7 @@ import fetch, { actionResponse } from "@/lib/fetch";
 const Confirm = ({ aps, isOpen, setIsOpen, token, value }) => {
   const t = useTranslations("Dialog");
   const handleConfirm = async () => {
-    console.log("Confirm", value);
+    // console.log("Confirm", value);
     const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${aps}/operation/pp`;
     const json = await fetch(url, {
       method: "POST",
@@ -50,14 +50,13 @@ export default function Operation({ action, aps, disabled, token }) {
   const t = useTranslations("Dialog");
   const [isOpen, setIsOpen] = useState(false);
   const [error, setError] = useState({ status: false, message: "" });
-  const [dest, setDest] = useState(1);
+  const [dest, setDest] = useState(0);
   // Confirm dialog
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
   const [value, setValue] = useState({ key: "", value: "" });
 
   const handleConfirm = async (item) => {
-    console.log(item);
-    // setValue(item)
+    // console.log(item);
     if (item.key === "B") {
       setValue({ key: item.key, value: item.value });
     } else {
@@ -91,54 +90,60 @@ export default function Operation({ action, aps, disabled, token }) {
       <Dialog
         isOpen={isOpen}
         title={t("PP.dialogTitle", { operation: t(action.key) })}
-        onClose={() => console.log("close")}
       >
-        <Divider />
-        <div className="grid grid-cols-8 gap-1 mt-6">
-          {action.buttons.map((item, key) => {
-            if (item.key === "B")
-              return (
-                <Tooltip tooltip={item.tooltip}>
-                  <Button variant="primary" onClick={() => handleConfirm(item)}>
-                    {item.value}+{item.key}
-                  </Button>
-                </Tooltip>
-              );
-          })}
-        </div>
-        <Divider>operations</Divider>
-        {/* <Text className="mt-6">
-          {t("PP.dialogText", { min: action.min, max: action.max })}
-        </Text> */}
-        <NumberInput
-          className="mt-3"
-          placeholder={t("PP.dialogTextInput", {
-            min: action.min,
-            max: action.max,
-          })}
-          error={error.status}
-          errorMessage={error.message}
-          min={action.min}
-          max={action.max}
-          dest={dest}
-          onValueChange={handleOnValueChange}
-        />
-        <div className="flex gap-3 mt-6">
-          {action.buttons.map((item, key) => {
-            if (item.key === "D" || item.key === "E" || item.key === "F")
-              return (
-                <Tooltip tooltip={item.tooltip}>
-                  <Button
-                    variant="primary"
-                    onClick={() => handleConfirm(item)}
-                    disabled={error.status}
-                  >
-                    DESTINATION + {item.key}
-                  </Button>
-                </Tooltip>
-              );
-          })}
-        </div>
+        {action.buttons.some((b) => b.key === "B") && (
+          <div className="grid grid-cols-8 gap-1 mt-6">
+            {action.buttons.map((item, key) => {
+              if (item.key === "B")
+                return (
+                  <Tooltip tooltip={item.tooltip}>
+                    <Button
+                      variant="primary"
+                      onClick={() => handleConfirm(item)}
+                    >
+                      {item.value}+{item.key}
+                    </Button>
+                  </Tooltip>
+                );
+            })}
+          </div>
+        )}
+        {action.buttons.some((b) => b.key !== "B") && (
+          <div className="mt-6">
+            {/* <Text className="mt-6">{t("PP.dialogText", { min: action.min, max: action.max })}</Text> */}
+            <NumberInput
+              className="mt-3"
+              placeholder={t("PP.dialogTextInput", {
+                min: action.min,
+                max: action.max,
+              })}
+              error={error.status}
+              errorMessage={error.message}
+              min={action.min}
+              max={action.max}
+              dest={dest}
+              onValueChange={handleOnValueChange}
+            />
+            <div className="flex gap-3 mt-6">
+              {action.buttons.map((item, key) => {
+                if (item.key === "D" || item.key === "E" || item.key === "F")
+                  return (
+                    <Tooltip tooltip={item.tooltip}>
+                      <Button
+                        variant="primary"
+                        onClick={() => handleConfirm(item)}
+                        disabled={
+                          !(dest > 0 && dest <= action.max) || error.status
+                        }
+                      >
+                        DESTINATION + {item.key}
+                      </Button>
+                    </Tooltip>
+                  );
+              })}
+            </div>
+          </div>
+        )}
         <Flex justifyContent="end" className="space-x-3 mt-6">
           <Button variant="secondary" onClick={() => setIsOpen(false)}>
             {t("cancel")}
